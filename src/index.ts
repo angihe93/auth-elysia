757ed9d9d00f782ad91a2449251642fff0e39ce0
angihe93 <angelhe903@gmail.com>
2025-06-11 13:06:18 -0400

    step 1-2

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,16 @@
 import { Elysia } from "elysia";
+import swagger from "@elysiajs/swagger"
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const PORT = 3000
+
+const app = new Elysia()
+  .use(swagger({
+    path: '/api-docs'
+  }))
+  .get("/", () => "Hello Elysia")
+  .get("/api/public", () => { return { message: "This is public information" } })
+  .get("/api/protected", () => { return { message: "Only admin should be able to see this" } })
+  .listen(PORT);
 
 console.log(
   `🦊 Elysia is running at http://${app.server?.hostname}:${app.server?.port}`
