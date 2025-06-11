c9ed88858db5b04832a9c169f62f40c5a3c057d8
angihe93 <angelhe903@gmail.com>
2025-06-11 17:48:08 -0400

    set jwt in auth cookie in /api/login

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,16 @@
 import { Elysia, t, status } from "elysia";
 import swagger from "@elysiajs/swagger"
+import { jwt } from '@elysiajs/jwt'
 
 const PORT = 3000
+const JWT_SECRET = process.env.JWT_SECRET as string // generated with https://dev.to/tkirwa/generate-a-random-jwt-secret-key-39j4
+if (!JWT_SECRET) {
+  throw new Error("JWT_SECRET is not set");
+}
+const COOKIE_SECRET = process.env.COOKIE_SECRET
+if (!COOKIE_SECRET) {
+  throw new Error("COOKIE_SECRET is not set");
+}
 
 // test users
 const users = [
@@ -9,7 +18,12 @@ const users = [
   { id: 2, username: "user", password: "user123", role: "basic", secret: "user-secret-456" }
 ];
 
-const app = new Elysia()
+const app = new Elysia({
+  cookie: { // https://elysiajs.com/patterns/cookie.html#constructor
+    secrets: COOKIE_SECRET,
+    sign: ['auth']
+  }
+})
   .use(swagger({
     path: '/api-docs'
   }))
@@ -36,6 +50,41 @@ const app = new Elysia()
       }
     }
   )
+
+  .use(
+    jwt({
+      name: 'jwt',
+      secret: JWT_SECRET,
+      exp: '1h'
+    })
+  )
+  .post('/api/login',
+    async ({ jwt, body, cookie: { auth } }) => {
+      const username = body.username
+      const password = body.password
+      const result = users.filter((u) => u.username === username && u.password === password)
+      if (!result.length)
+        return status(401)
+      const role = result[0].role
+      const id = result[0].id
+      console.log(username, password, id, role)
+      // sign jwt
+      const signedJwt = await jwt.sign({ id, role, exp: '1h' })
+      console.log(signedJwt)
+      // set auth cookie:
+      auth.set({ value: signedJwt, expires: new Date(Date.now() + (3600 * 1000 * 24)) })
+      console.log(auth)
+    },
+    { // validate types
+      body: t.Object({
+        username: t.String(),
+        password: t.String()
+      }),
+      response: t.Object({
+      }, { description: 'a JWT will be set in cookies after successful login' })
+    }
+  )
+
   .listen(PORT);
 
 console.log(
